a27405e7a7f2e6361a7db5f464b7672ec0fa7b71
gluknik <gluknik@gmail.com>
2022-06-15 23:12:41 +0300

    Adds a few sections

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,9 @@ import type { NextPage } from "next"
 import Layout from "../components/layout"
 import Hero from "../components/sections/hero"
 import Info from "../components/sections/info"
+import BuyATicket from "../components/sections/buy-a-ticket"
+import HowToPlay from "../components/sections/how-to-play"
+import Roadmap from "../components/sections/roadmap"
 // import { Text, Title, Anchor, Button, Group, Badge, Box, useMantineTheme } from "@mantine/core"
 // // import { Code, Rocket } from "tabler-icons-react"
 // import Link from "next/link"
@@ -12,6 +15,9 @@ const Home: NextPage = () => {
       <>
         <Hero />
         <Info />
+        <BuyATicket />
+        <HowToPlay />
+        <Roadmap />
       </>
     </Layout>
   )
