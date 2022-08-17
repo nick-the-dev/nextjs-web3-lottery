545866839acfdf11fd9f5b9757b468490c448143
gluknik <gluknik@gmail.com>
2022-08-17 19:08:19 +0300

    Devides to bnb and matic version

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,22 +4,10 @@ import Head from "next/head"
 // global styles
 import "../styles/globals.scss"
 // mantine theming
-import yourMantineTheme from "../themes/mantine"
-import { MantineProvider, ColorScheme, ColorSchemeProvider } from "@mantine/core"
-import { WalletContextWrapper } from "../context/wallet.context"
-import { NotificationsProvider } from "@mantine/notifications"
+
 import "../styles/styles.scss"
 
-export default function App(props: AppProps & { colorScheme: ColorScheme }) {
-  const { Component, pageProps } = props
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme)
-
-  const toggleColorScheme = (value?: ColorScheme) => {
-    const nextColorScheme = value || (colorScheme === "dark" ? "light" : "dark")
-    setColorScheme(nextColorScheme)
-    // NOTE: if you want to, set cookie here
-  }
-
+export default function App() {
   return (
     <>
       <Head>
@@ -28,34 +16,11 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <meta name="description" content="Don't miss the opportunity to easily win 1 ETH" />
       </Head>
 
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-          theme={{
-            ...yourMantineTheme,
-            colorScheme,
-            // you can change primaryColor w.r.t colorScheme here
-          }}
-          defaultProps={{
-            Container: {
-              sizes: {
-                xs: 540,
-                sm: 720,
-                md: 960,
-                lg: 1140,
-                xl: 1320,
-              },
-            },
-          }}
-        >
-          <WalletContextWrapper>
-            <NotificationsProvider>
-              <Component {...pageProps} />
-            </NotificationsProvider>
-          </WalletContextWrapper>
-        </MantineProvider>
-      </ColorSchemeProvider>
+      <div>
+        <a href="/bnb">BNB</a>
+        <br />
+        <a href="/bnb">Polygon</a>
+      </div>
     </>
   )
 }
